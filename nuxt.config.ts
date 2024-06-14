ac9a0ca8a90bd8d95dc0b26300e0d1ad6a63287b
llladno <man30968@gmail.com>
2024-06-14 12:04:17 +0300

    final commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  router: {
-    base: '/opt6/',
+  app: {
+    baseURL: '/opt6/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
   }
 })
